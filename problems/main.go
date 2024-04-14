6526b9940bd9e785db6025d136c5db15a2919d5f
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-04-14 21:17:06 +0900

    [443] String Compression

--- problems/main.go
+++ problems/main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println(canPlaceFlowers([]int{0, 0, 1, 0, 0}, 1))
+	s := []string{"a", "a", "a", "b", "b", "a", "a"}
+	bs := []byte{}
+	for _, ss := range s {
+		fmt.Printf("[]byte(ss): %v\n", []byte(ss))
+		b := []byte(ss)
+		bs = append(bs, b[0])
+	}
+	fmt.Printf("bs: %v\n", bs)
+
+	fmt.Println(compress(bs))
+	fmt.Printf("bs: %v\nv: ", bs)
+	for _, v := range bs {
+		fmt.Printf("%v, ", string(v))
+	}
+	fmt.Println("")
 }
